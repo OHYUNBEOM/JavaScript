7cc00542b931d43d6fa4efef53517defd462cd4a
OHYUNBEOM <dbsqja353@naver.com>
2021-04-07 18:31:02 +0900

    datatype of js

---
+++ datatype.js
@@ -0,0 +1,16 @@
+console.log(1);
+console.log(1+1);
+console.log(6/2);
+console.log(2*8);
+
+console.log("Hello World");
+console.log("'Hello World'");
+//' 출력하고싶을때 ' 로 끝내면됨
+console.log("Hello World".length);
+//문자열의 길이
+console.log("helloworld".toUpperCase());
+//소문자의 대문자화
+console.log('helloworld'.indexOf('o'));
+//o 의 위치 index 위치
+console.log('         hello         ');
+console.log('         hello         '.trim());
